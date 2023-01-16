e637ea3dd1248362e099548628506f0edc684772
omar <66186617+o0mrs@users.noreply.github.com>
2023-01-16 15:23:54 +0300

    Add files via upload

--- src/com/home.jsx
+++ src/com/home.jsx
@@ -7,15 +7,16 @@ roboto
 import './css/App.css'
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from 'react';
+import NavBar from './parts/navbar';
 const Home = ()=>{
 let navigate = useNavigate();
 let tab = 0
 if(tab == 0){
     return(
-<div>
-  <div className="Poppins text-5xl h-[90vh]  animate-bounce grid place-items-center">
-  OMAR
-  </div>
+      
+<div className='bga h-[220rem]'>
+<div className="dvb-cursor -visible" style={{translate: 'none', rotate: 'none', scale: 'none', transform: 'translate3d(336px, 233px, 0px)'}}><div className="dvb-cursor-text" /><div className="dvb-cursor-ball" /></div>
+<NavBar />
 </div>
             )
 }else{
--- src/com/parts/navbar.jsx
+++ src/com/parts/navbar.jsx
@@ -7,9 +7,13 @@ const NavBar = ()=>{
   const [pfp, setpfp] = useState('./logo.png')
   let navigate = useNavigate();
     return(
-      <div className="navbar text-white">
-  <div className="navbar-start">
-    <div className="dropdown  text-white">
+      <div className="navbar lg:px-20 md:px-16 sm:px-4  text-white">
+  <div className="navbar-start  h-12">
+    {/* logo */}
+    <div className="Quicksand text-4xl ">
+q
+    </div>
+    {/* <div className="dropdown  text-white">
     <label tabIndex={0} className="btn btn-ghost btn-circle">
     <i class="fa-solid fa-bars-staggered text-xl"></i>      </label>
       <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-300 rounded-box w-52">
@@ -18,19 +22,28 @@ const NavBar = ()=>{
         <li onClick={()=>{navigate('/',{replace:false})}}><a>Projects</a></li>
         <li onClick={()=>{navigate('/',{replace:false})}}><a>contact</a></li>
       </ul>
-    </div>
+    </div> */}
   </div>
   <div className="navbar-center">
-    <a onClick={()=>{navigate('/go',{replace:false})}} className="btn btn-ghost text-white b normal-case text-2xl">Enigma</a>
+    {/* <a onClick={()=>{navigate('/go',{replace:false})}} className="btn btn-ghost text-white b normal-case text-2xl">Enigma</a> */}
   </div>
-  <div className="navbar-end">
- 
-    <button className="btn btn-ghost btn-circle">
-      <div className="indicator">
-      <img className="rounded-full h-8 w-8" src={'/main.png'}></img>
+  <div className="navbar-end  h-12  pt-[1.3rem] text-2xl">
+  <div className="Quicksand px-4  ">
+      {/* <i class="fa-solid fa-bars"></i> */}
+      <i class="fa-solid fa-magnifying-glass"></i>
+    </div>
+    <div className="Quicksand px-4  ">
+      {/* <i class="fa-solid fa-bars"></i> */}
+      <i class="fa-solid fa-briefcase"></i>
+    </div>
+  <div className="Quicksand px-4  ">
+      {/* <i class="fa-solid fa-bars"></i> */}
+      Login
+    </div>
+    <div className=" hover:bg-white hover:text-black p-4 rounded-full px-5">
+      <i class="fa-solid fa-bars"></i>
+    </div>
 
-      </div>
-    </button>
   </div>
 </div>
 
